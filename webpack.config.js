ef57c0a63be5cba3170f41ba28295bd15ebee4c3
Kyle Warren <qarren@gmail.com>
2016-02-17 16:57:45 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        text: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime'],
+        },
+      },
+    ],
+  },
+};
+
